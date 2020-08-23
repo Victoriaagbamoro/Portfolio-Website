027fe54590fa751df9701fddb56e6897bc722ed6
Agbamoro Victoria <45354743+vickyagba@users.noreply.github.com>
2020-08-23 14:13:45 +0100

    updatedpages

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ gsap.to('.layer-1', {y: '-100vh', delay: .5});
 
     gsap.fromTo('.home-area',
         {x: '30rem', opacity: 0},
-        {x: 0, opacity: 1, ease: 'back.out(1.7)', delay:1.1g});
+        {x: 0, opacity: 1, ease: 'back.out(1.7)', delay:1.1});
 
     
