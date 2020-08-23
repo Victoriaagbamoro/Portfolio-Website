b4589ea97c8484278b8b324dc82d24fc81e2a372
Agbamoro Victoria <45354743+vickyagba@users.noreply.github.com>
2020-08-23 01:30:27 +0100

    porfolio

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-$(document).ready(function () {
+gsap.to('.layer-1', {y: '-100vh', delay: .5});
+    gsap.to('.layer-2', {y: '-100vh', delay: .7});
+    gsap.to('.layer-3', {y: '-100vh', delay: .9});
+    gsap.to('.overlay', {y: '-100vh', delay: 1.5});
 
-});
+    gsap.fromTo('.home-area',
+        {x: '30rem', opacity: 0},
+        {x: 0, opacity: 1, ease: 'back.out(1.7)', delay:1.4});
+
+    gsap.fromTo('.about-area',
+        {x: '30rem', opacity: 0},
+        {x: 0, opacity: 1, ease: 'back.out(1.7)', delay:2.0});
+
+    gsap.fromTo('.project-area',
+        {x: '-30rem', opacity: 0},
+        {x: 0, opacity: 1, ease: 'back.out(1.7)', delay:1});
+
+    gsap.fromTo('.blog-area',
+        {x: '30rem', opacity: 0},
+        {x: 0, opacity: 1, ease: 'back.out(1.7)', delay:1.5});
